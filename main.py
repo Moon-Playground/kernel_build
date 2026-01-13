b02376a0bc811e8c426060a3db02216991d6d86f
月 <wulan17@komodos.id>
2026-01-14 02:43:55 +0700

    remove CONFIG_KSU_SUSFS_TRY_UMOUNT

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ def append_config(config_name):
             defconfig_file.write('\n# KernelSU\nCONFIG_KERNELSU=y\nCONFIG_KSU_MANUAL_HOOK=y\n')
     elif config_name == 'susfs':
         with open(DEFCONFIG_PATH, "a", encoding="utf-8") as defconfig_file:
-            defconfig_file.write('\n# KernelSU - SusFs\nCONFIG_KSU_SUSFS=y\nCONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT=y\nCONFIG_KSU_SUSFS_SUS_PATH=y\nCONFIG_KSU_SUSFS_SUS_MOUNT=y\nCONFIG_KSU_SUSFS_SUS_KSTAT=y\nCONFIG_KSU_SUSFS_TRY_UMOUNT=y\nCONFIG_KSU_SUSFS_SPOOF_UNAME=y\nCONFIG_KSU_SUSFS_ENABLE_LOG=y\nCONFIG_KSU_SUSFS_OPEN_REDIRECT=y\n')
+            defconfig_file.write('\n# KernelSU - SusFs\nCONFIG_KSU_SUSFS=y\nCONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT=y\nCONFIG_KSU_SUSFS_SUS_PATH=y\nCONFIG_KSU_SUSFS_SUS_MOUNT=y\nCONFIG_KSU_SUSFS_SUS_KSTAT=y\nCONFIG_KSU_SUSFS_SPOOF_UNAME=y\nCONFIG_KSU_SUSFS_ENABLE_LOG=y\nCONFIG_KSU_SUSFS_OPEN_REDIRECT=y\n')
 
 def upload(file_name, url):
     load_config()
