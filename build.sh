9f27065fd8a44f0a54c411f89e62f22f0a934fab
wulan17 <wulan17@komodos.id>
2026-07-15 21:33:32 +0700

    gale: Switch to ReSukiSU
    Signed-off-by: wulan17 <wulan17@komodos.id>

--- build.sh
+++ build.sh
@@ -41,12 +41,10 @@ function do_kernel(){
 	git -C kernel config --local user.email "$KBUILD_BUILD_USER@example.com"
 
 	if [[ "$B_TYPE" == "ksu" ]]; then
-		git -C kernel am --3way "$BASE_DIR"/patches/0001-KernelSU-Patch.patch || { echo "Patch application failed!"; exit 1; }
+		git -C kernel am --3way "$BASE_DIR"/patches/0001-gale-ReSukiSU-manual-hook.patch || { echo "Patch application failed!"; exit 1; }
 		python main.py append_config "ksu"
 	elif [[ "$B_TYPE" == "susfs" ]]; then
-		git -C kernel am --3way "$BASE_DIR"/patches/0001-KernelSU-Patch.patch || { echo "Patch application failed!"; exit 1; }
-		git -C kernel am --3way "$BASE_DIR"/patches/0002-Susfs-Patch.patch || { echo "Patch application failed!"; exit 1; }
-		python main.py append_config "ksu"
+		git -C kernel am --3way "$BASE_DIR"/patches/0001-gale-ReSukiSU-susfs.patch || { echo "Patch application failed!"; exit 1; }
 		python main.py append_config "susfs"
 	fi
 	python main.py update_localversion
